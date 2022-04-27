8d2b93f2e46e0a2ed579b00871b0f4ae5191a7d8
guyllaumedemers <guyllaume_demers@hotmail.fr>
2022-04-27 19:16:24 -0400

    init

---
+++ SDL2-Minesweeper/Main.cpp
@@ -0,0 +1,11 @@
+﻿// SDL2-Minesweeper.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+
+using namespace std;
+int main(int argc, char* argv[])
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
